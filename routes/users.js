933ad977c8da0d5698c76757632fd603160ffe35
Mohammad Khalilzadeh <46302149+MohammadKhalilzadeh@users.noreply.github.com>
2022-03-10 17:45:48 +0330

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -1,43 +1,148 @@
+const { response } = require('express')
 const express = require('express')
+const { where } = require('../models/user')
 const router = express.Router()
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const checkAuth = require('../middleware/check-auth')
 
-router.get("/", (req, res) => {
-    res.send("User List")
-})
 
-router.get("/new", (req, res) => {
-    res.render("users/new")
-})
-
-router.post('/', (req, res) => {
-    const isValid = true
-    if (isValid) {
-        users.push({ firstName: req.body.firstName })
-        res.redirect('/users/${users.length - 1}')
-    } else {
-        console.log("Error");
-        res.redirect('/users/new', { firstName: req.body.firstName })
+// Getting all
+router.get('/',/* checkAuth, */ async (req, res) => {
+    try{
+        const users = await User.find()
+        return res.status(200).json(users)
+    }catch (err) {
+        res.status(500).json({ message: err.message })
     }
-    res.send("Create User")
 })
 
-router.get('/:id', (req, res) => {
-    console.log(req.user);
-    res.send('Get user with ID ${req.params.id}')
+// Getting One
+router.get('/:id', getUser,/* checkAuth,*/ async  (req, res) => {
+    return res.status(200).send(res.user)
 })
 
-router.put('/:id', (req, res) => {
-    res.send('Update user with ID ${req.params.id}')
+// Get by Email or Phone
+router.post('/single/:input', async  (req, res) => {
+    console.log(req.params.input);
+    User.findOne({ $or:[ { phone: req.params.input },{email:req.params.input} ]})
+    .exec()
+    .then(user => {
+        console.log(user);
+        console.log(req.body);
+        console.log("1");
+        if(!user){
+            console.log("2");
+            return res.status(404).json({
+                message: 'Not Found'
+            })
+        }
+        bcrypt.compare(req.body.password,user.password,(err,result)=>{
+            if(err){
+                return res.status(401).json({
+                    message: 'Unauthorized'
+                })
+            }
+            if(result){
+            const token = jwt.sign({
+                "id":user._id,
+            } ,
+            "tm4ever"
+            ,{ expiresIn: "1h" }, )
+                return res.status(202).json({token:token})
+            }
+            //  res.status(401).json({
+            //     message: 'Auth Failed'
+            // })
+            // console.log("BCRYPT");
+            // console.log("5");
+        })
+    })
+    .catch(err => {
+        res.status(500).json({ error: err})
+    })
+
 })
 
-router.delete('/:id', (req, res) => {
-    res.send('Delete user with ID ${req.params.id}')
+// Creating One
+router.post('/',  async (req, res) => {
+
+    try {
+        User.find({email: req.body.email})
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: 'Conflict'
+                })
+            } else {
+                bcrypt.hash(req.body.password,10, async (err,hash) => {
+                    if(err){
+                        return res.status(503).json({
+                            error:err
+                        })
+                    }else{
+                        const user = new User({
+                            firstname: req.body.firstname,
+                            lastname: req.body.lastname,
+                            email: req.body.email,
+                            phone: req.body.phone,
+                            password:hash ,
+                        })
+                        //const newuser =
+                        await user.save().then(result => {
+                            res.status(201).json(result)
+                        }).catch(err => {
+                            console.log(err);
+                            res.status(500).json({ error: err})
+                        })
+                        // res.status(200).json(newuser)
+                    }
+                })
+            }
+        })
+        .catch()
+
+    } catch (error) {
+        res.status(400).json({message:error.message})
+    }
 })
 
-const users = [{ name: "Kyle"}, { name: "Sally"}]
-router.param("id", (req,res, next, id) => {
-    req.user = users[id]
+// Updating One
+router.patch('/:id', getUser, checkAuth,  async  (req, res) => {
+    try {
+        const updatedUser = await res.user.save()
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        res.status(400).json({ message: error.message})
+    }
+})
+
+// Deleting One
+router.delete('/:id', getUser, checkAuth,  async (req, res) => {
+    try {
+        await res.user.remove()
+        res.status(200).json({ message: 'Deleted user' })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+async function getUser(req, res, next) {
+    let user
+    try {
+        console.log(req.params.id);
+        user = await User.findById(req.params.id)
+        if (user == null){
+            return res.status(404).json({message: 'Not Found'})
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+
+    res.user = user
     next()
-})
+}
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,29 +1,32 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
-
-app.use(express.static("public"))
-app.use(express.urlencoded({ extended: true }))
+const cors = require("cors")
+const mongoose = require('mongoose')
 app.use(express.json())
 
-app.set("view engine", "ejs")
+app.use(
+    cors({
+        origin:"*"
+    })
+)
 
-app.get('/', logger, (req, res) => {
-    console.log("Hello");
+app.use('/images',express.static('images'))
 
-    // res.sendStatus(500);
-    // res.status(500).send("Hello")
-    // res.status(500).json({ message: "Error"})
-    // res.download('filename.js')
-    res.render("index")
-})
+app.set('view engine','ejs')
 
-const userRouter = require("./routes/users")
-app.use("/users", userRouter)
+mongoose.connect( process.env.DATABASE_URL, {useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
-function logger(req, res, next) {
-    console.log(req.originalUrl)
-    next()
-}
 
-app.listen(3000)
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
+app.listen(3000, () => console.log("Server Started") )
+
+module.exports = app;
